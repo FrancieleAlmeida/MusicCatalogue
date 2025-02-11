0b735020712d70227d4e5ee7314193de57c8a507
FrancieleAlmeida <francielytlc3@gmail.com>
2025-02-11 11:46:04 -0400

    atualizando logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import Logo from '../assets/deezer.png';
+
 
 const Navbar = () => {
   return (
     <nav className="bg-primary text-white p-4 flex justify-between items-center border-b border-gray-700">
       <Link to="/" className="flex items-center gap-2">
-        <img src="../src/assets/deezer.png" alt="Logo" className="h-6" />
+        <img src={Logo} alt="Logo" className="h-6" />
         <span className="text-xl font-bold">MusicApp</span>
       </Link>
       <ul className="flex gap-4">
